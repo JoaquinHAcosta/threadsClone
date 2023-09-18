5e0dab0714de6b9e5b508fba7a3e9610d1720f56
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-09-17 23:22:48 -0300

    styled threadcard, position need to be fixed

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+import Image from "next/image"
 
 interface Props {
     id: string
@@ -34,10 +36,31 @@ const ThreadCard = ({
     comments,
 }: Props) => {
     return (
-        <article>
-            <h2 className='text-small-regular text-light-2'>
-                {content}
-            </h2>
+        <article className='flex w-full flex-col rounded-xl bg-dark-2 p-7'>
+            <div className='flex items-start justify-between'>
+                <div className='flex w-full flex-1 flex-row gap-4'>
+                    <div className='flex flex-col items-center'>
+                        <Link href={`/profile/${author.id}`} className='relative h-11 w-11'>
+                            <Image
+                                src={author.image}
+                                alt='Profile image'
+                                fill
+                                className='cursor-pointer rounded-full'
+                            />
+                        </Link>
+
+                        <div className='thread-card_bar'/>
+
+                        <div className='flex w-full flex-col'>
+                            <Link href={`/profile/${author.id}`} className='w-fit'>
+                                <h4 className="cursor-pointer text-base-semibold text-light-1">{author.name}</h4>
+                            </Link>
+
+                            <p className='mt-2 text-small-regular text-light-2'>{content}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </article>
     )
 }
