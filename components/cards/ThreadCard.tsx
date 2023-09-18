daca52e3613c99ece62d999c8dc154453efbc22e
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-09-18 00:16:58 -0300

    thread of threads created

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -34,6 +34,7 @@ const ThreadCard = ({
     community,
     createdAt,
     comments,
+    isComment
 }: Props) => {
     return (
         <article className='flex w-full flex-col rounded-xl bg-dark-2 p-7'>
@@ -57,6 +58,28 @@ const ThreadCard = ({
                             </Link>
 
                             <p className='mt-2 text-small-regular text-light-2'>{content}</p>
+
+                            <div className='mt-5 flex flex-col gap-3'>
+                                {/* as an extra i should add funcionality from this buttons */}
+                                <div className='flex gap-3.5'>
+                                    <Image src='/assets/heart-gray.svg' alt='hearth' width={24} height={24} 
+                                    className='cursor-pointer object-contain'/>
+                                    <Link href={`/thread/${id}`}>
+                                        <Image src='/assets/reply.svg' alt='reply' width={24} height={24} 
+                                        className='cursor-pointer object-contain'/>
+                                    </Link>
+                                    <Image src='/assets/repost.svg' alt='repost' width={24} height={24} 
+                                    className='cursor-pointer object-contain'/>
+                                    <Image src='/assets/share.svg' alt='share' width={24} height={24} 
+                                    className='cursor-pointer object-contain'/>
+                                </div>
+
+                                {isComment && comments.length > 0 && (
+                                    <Link href={`/thread/${id}`}>
+                                        <p className='mt-1 text-subtle-medium text-gray-1'>{comments.length}</p>
+                                    </Link>
+                                )}
+                            </div>
                         </div>
                     </div>
                 </div>
